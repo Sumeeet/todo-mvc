ac867dab3a9478b62f32cbf7a66b6fd8c4d4a5d0
sukhanna <sumeet.friends@gmail.com>
2020-01-18 20:07:18 +0530

    mvc todo

---
+++ main.js
@@ -0,0 +1,188 @@
+'use strict'
+
+class Model {
+    constructor() {
+        this.todos = [
+            { id: 0, text: 'Run a marathon', complete: false },
+            { id: 1, text: 'Plant a tree', complete: false }
+        ]
+    }
+
+    bindTodoListChanged(callback) {
+        this.onTodoListChanged = callback;
+    }
+
+    addTodo(todoText) {
+        const todo = {
+            id: this.todos.length,
+            text: todoText,
+            complete: false
+        }
+
+        this.todos.push(todo);
+        this.onTodoListChanged(this.todos);
+    }
+
+    editTodo(id, updatedText) {
+        this.todos[id].text = updatedText;
+        this.onTodoListChanged(this.todos);
+    }
+
+    deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        this.onTodoListChanged(this.todos);
+    }
+
+    toggleTodo(id) {
+        this.todos[id].complete = !this.todos[id].complete;
+        this.onTodoListChanged(this.todos);
+    }
+}
+
+class View {
+    constructor() {
+        this.app = this.getElement('#root');
+
+        this.title = this.createElement('h1');
+        this.title.textContent = 'ToDos';
+
+        this.form = this.createElement('form');
+        this.input = this.createElement('input')
+        this.input.type = 'text';
+        this.input.placeholder = "Add ToDo";
+        this.input.name = 'todo';
+
+        this.submitButton = this.createElement('button');
+        this.submitButton.textContent = 'Submit';
+
+        this.todoList = this.createElement('ul', 'todo-list');
+
+        this.form.append(this.input, this.submitButton);
+
+        this.app.append(this.title, this.form, this.todoList);
+
+    }
+
+    createElement(tag, className) {
+        let element = document.createElement(tag);
+        if (className) element.classList.add(className);
+        return element;
+    }
+
+    getElement(selector) {
+        return document.querySelector(selector);
+    }
+
+    get _todoText() {
+        return this.input.value
+    }
+
+    _resetInput() {
+        this.input.value = ''
+    }
+
+    displayTodos(todos) {
+        // Delete all nodes
+        while (this.todoList.firstChild) {
+            this.todoList.removeChild(this.todoList.firstChild)
+        }
+
+        if (todos.length === 0) {
+            const p = this.createElement('p');
+            p.textContent = 'Nothing to do. Add a task!';
+            this.todoList.append(p);
+        }
+        else {
+            todos.forEach(todo => {
+                const li = this.createElement('li');
+                li.id = todo.id;
+
+                const checkBox = this.createElement('input');
+                checkBox.type = 'checkbox';
+                checkBox.checked = todo.complete;
+
+                const span = this.createElement('span');
+                span.contentEditable = true;
+                span.classList.add('editable');
+
+                if (todo.complete) {
+                    const strike = this.createElement('s');
+                    strike.textContent = todo.text;
+                    span.append(strike);
+                }
+                else {
+                    span.textContent = todo.text;
+                }
+
+                const deleteButton = this.createElement('button', 'delete');
+                deleteButton.textContent = 'Delete';
+                li.append(checkBox, span, deleteButton);
+
+                // Append nodes
+                this.todoList.append(li);
+            });
+        }
+    }
+
+    bindAddTodo(handler) {
+        this.form.addEventListener('click', event => {
+            event.preventDefault();
+            if (this._todoText) {
+                handler(this._todoText);
+            }
+        });
+    }
+
+    bindDeleteTodo(handler) {
+        this.todoList.addEventListener('click', event => {
+            if (event.target.className === 'delete') {
+                const id = parseInt(event.target.parentElement.id);
+                handler(id);
+            }
+        });
+    }
+
+    bindToggleTodo(handler) {
+        this.todoList.addEventListener('change', event => {
+            if (event.target.type === 'checkbox') {
+                const id = parseInt(event.target.parentElement.id);
+                handler(id);
+            }
+        });
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        this.model.bindTodoListChanged(this.onTodoListChanged);
+        this.view.bindAddTodo(this.handleAddTodo);
+        this.view.bindDeleteTodo(this.handleDeleteTodo);
+        this.view.bindToggleTodo(this.handleToggleTodo);
+        this.onTodoListChanged(this.model.todos);
+    }
+
+    onTodoListChanged = todos => {
+        this.view.displayTodos(todos);
+    }
+
+    handleAddTodo = todoText => {
+        this.model.addTodo(todoText)
+    }
+
+    handleEditTodo = (id, todoText) => {
+        this.model.editTodo(id, todoText)
+    }
+
+    handleDeleteTodo = id => {
+        this.model.deleteTodo(id)
+    }
+
+    handleToggleTodo = id => {
+        this.model.toggleTodo(id)
+    }
+}
+
+const app = new Controller(new Model(), new View());
